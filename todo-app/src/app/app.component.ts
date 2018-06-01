1804e914e32ee3eea650b003eb6d94feda96d8b8
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-06-01 11:17:50 -0400

    set searchValue to null after enter or clicking add button to clear field

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -10,9 +10,12 @@ export class AppComponent {
 
   todoArray=[]
 
+  searchValue:string = '';
+
   addTodo(value) {
     this.todoArray.push(value)
     console.log(this.todoArray)
+    this.searchValue = null;
   }
 
   deleteItem(todo){
@@ -28,6 +31,7 @@ export class AppComponent {
     if (value!== "") {
       this.todoArray.push(value)
       console.log(this.todoArray)
+      this.searchValue = null;
     } else {
       alert("Please fill out field before submitting")
     }
