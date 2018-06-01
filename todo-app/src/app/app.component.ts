a59ec4f84f60c1e0d931e9877dd2eeaafce329d4
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-05-31 23:39:07 -0400

    created animations within @Component for todo li items

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -4,7 +4,27 @@ import { Component,trigger,animate,style,transition,keyframes } from '@angular/c
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger("moveInLeft",[
+       transition("void=> *",[style({transform:"translateX(300px)"}),
+         animate(200,keyframes([
+          style({transform:"translateX(300px)"}),
+          style({transform:"translateX(0)"})
+  
+           ]))]),
+ 
+ 
+           transition("*=>void",[style({transform:"translateX(0px)"}),
+         animate(100,keyframes([
+          style({transform:"translateX(0px)"}),
+          style({transform:"translateX(300px)"})
+  
+           ]))])    
+      
+      ])
+ 
+   ]
 })
 export class AppComponent {
   title = 'app'
