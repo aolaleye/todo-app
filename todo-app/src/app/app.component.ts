e51fb1531f0bb3e7bf5e96c170ec06ca06e4bc26
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-06-01 10:41:01 -0400

    removed unused imports

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,30 +1,9 @@
-// import { Component } from '@angular/core';
-import { Component,trigger,animate,style,transition,keyframes } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations:[
-    trigger("moveInLeft",[
-       transition("void=> *",[style({transform:"translateX(300px)"}),
-         animate(200,keyframes([
-          style({transform:"translateX(300px)"}),
-          style({transform:"translateX(0)"})
-  
-           ]))]),
- 
- 
-           transition("*=>void",[style({transform:"translateX(0px)"}),
-         animate(100,keyframes([
-          style({transform:"translateX(0px)"}),
-          style({transform:"translateX(300px)"})
-  
-           ]))])    
-      
-      ])
- 
-   ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app'
