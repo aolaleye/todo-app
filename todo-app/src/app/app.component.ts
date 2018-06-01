1a3473bf4e8f14efa1a28d31cfe0ddbed301e89d
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-05-31 23:37:14 -0400

    added additional imports from @angular/core

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { Component,trigger,animate,style,transition,keyframes } from '@angular/core';
 
 @Component({
   selector: 'app-root',
